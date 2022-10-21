3ad16fd5af118e7818b8de80a6bb56366ed06282
phucdang24 <106759480+phucdang24@users.noreply.github.com>
2022-10-21 22:51:06 +0700

    Second changed

--- Exercise/OnWebsite/FencePainting.java
+++ Exercise/OnWebsite/FencePainting.java
@@ -6,9 +6,10 @@ public class FencePainting{
     int cost[][];   // Two-Way Array contains paint cost in order red, blue, black
     private static final Scanner sc = new Scanner(System.in);
     
-    private void menu () {
+    private static void menu () {
         System.out.println("1.Enter number of iron bar");
-        System.out.println("2.Enter number of iron bar");
+        System.out.println("2.Enter cost table");
+        System.out.println("3.Two-way matrix");
     }
 
     private void init () {
@@ -17,23 +18,82 @@ public class FencePainting{
     }
 
     private void enterCostTable () {
-        int i = 0;
-        int a = (n-1)-(n-i-1);
-        for(i = 0; i < n; i++){
-            System.out.printf("Cost [%d][%d]: ", i, a);
-            this.cost[i][a] = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 3; j++ ) {
+                System.out.printf("Cost [%d][%d]: ", i, j);
+                this.cost[i][j] = sc.nextInt();  
+            }
         }
+        System.out.println();
     }
 
+    private int calculateTheMinimumValue () {
+        int minimum;    // Minimum value
+        int sum = 0;    // Cost sum
+        int avoidDuplication = -1;
 
+        for(int i = 0; i < n; i++) {
+
+            minimum = this.cost[i][0];
+
+            for(int j = 0; j < 3; j++ ) {
+                if( avoidDuplication == j){
+                   continue; 
+                }
+                else if(minimum < this.cost[i][j]){
+                    minimum = this.cost[i][j];
+                    avoidDuplication = j;
+                }
+                else
+                    avoidDuplication = 0;
+
+                sum += minimum;
+            }
+        }
+        return sum;
+    }
+
+    private void twoWayMatrix () {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < 3; j++ ) {
+                System.out.printf("%d ", this.cost[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
-        
-        
-        
+        int option;
+        FencePainting x = new FencePainting();
+        do{
+            FencePainting.menu();
+            System.out.print("Enter option: ");
+            option = sc.nextInt();
+
+            switch(option){
+                case 0:
+                    break;
+                case 1:
+                    x.init();
+                    break;
+                case 2:
+                    x.enterCostTable();
+                    break;
+                case 3:
+                    x.twoWayMatrix();
+                    break;
+                default:
+                    option = 0;
+                    break;
+            }
+
+        }while( option != 0);        
 
         
-
+        // System.out.println("1.Enter number of iron bar");
+        // System.out.println("2.Enter cost table");
+        // System.out.println("3.Two-way matrix");
         
     }
 
