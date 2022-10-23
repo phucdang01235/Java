908622a83cb548ba4ac2c653f827ea1bddda3a41
phucdang24 <106759480+phucdang24@users.noreply.github.com>
2022-10-23 22:34:25 +0700

    Thay doi

--- Exercise/OnWebsite/VirtualOrderTrap.java
+++ Exercise/OnWebsite/VirtualOrderTrap.java
@@ -5,40 +5,45 @@ import java.util.List;
 
 public class VirtualOrderTrap {
     int n; // number of order need solve
-    
+    List<Integer> valuesOfOrder;
 
-    private VirtualOrderTrap (int n, List<Integer> valuesOfOrder) {
+    private VirtualOrderTrap (int n) {
         this.n = n;
         valuesOfOrder = new LinkedList<Integer>();
         valuesOfOrder.add(Integer.parseInt("100"));
         valuesOfOrder.add(Integer.parseInt("200"));
         valuesOfOrder.add(Integer.parseInt("300"));
         valuesOfOrder.add(Integer.parseInt("400"));
-        valuesOfOrder.add(Integer.parseInt("400"));
-        valuesOfOrder.add(Integer.parseInt("2000"));
+        valuesOfOrder.add(Integer.parseInt("100"));
+        //valuesOfOrder.add(Integer.parseInt("2000"));
      
     }
 
     
 
-    private double result (double k, List<Integer> valuesOfOrder) {
+    private double result () {
         // h is profit
+        double k = 0;
         double h = 0;
         for(int i = 1; i <= this.n; i++){
+            k -= valuesOfOrder.get(i-1);
+
             if(i % 5 == 0){
                 h =  Math.abs(k) * 0.1;
                 k = 0;
             }
-            k -= valuesOfOrder.get(i - 1);
+            
         }
 
         return (k + h);
     }
 
     public static void main(String[] args) {
-        List<Integer> valuesOfOrder = null;
-        VirtualOrderTrap a = new VirtualOrderTrap(6, valuesOfOrder);
+    
+        VirtualOrderTrap a = new VirtualOrderTrap(5);
+
+        double b = a.result();
         
-        
+        System.out.println(b);
     }
 }
