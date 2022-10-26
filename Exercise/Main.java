dd329b01fed66952c6613a991b9464c68e13744d
phucdang24 <106759480+phucdang24@users.noreply.github.com>
2022-10-26 15:30:15 +0700

    huge changes

---
+++ Exercise/Main.java
@@ -0,0 +1,10 @@
+import hacker_rank.determining_adn_health.DeterminingADNHealth;
+
+public class Main{
+	
+	public static void main(String[] args) {
+		DeterminingADNHealth test = new DeterminingADNHealth(6,3);
+		test.setGene();
+		
+	}
+}
